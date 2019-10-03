8ae3173ba23688391b2382608fc9cfc8557df433
David <david.bland@hotmail.com>
2019-10-03 09:30:31 -0400

    changed hero background color and added m508 instead of m330. Also updated the size select in the buy menu

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
     //Code to control the buy modal
     var buyBtn = document.querySelector(".buyBubble");
+    var buyAmazon = document.querySelector(".buyBtn");
     var modal = document.querySelector(".modal");
     var mpStyle = document.querySelector(".styleContainer");
     var previewModel = document.querySelector(".previewSelectContainer");
@@ -83,6 +84,8 @@ document.addEventListener('DOMContentLoaded', function(){
         displayStyle(style);
     })
 
+    buyAmazon.addEventListener('click', goToAmazon);    
+
 
     //build a string based on the style object that will navigate to the right image folder / file 
     //to be displayed in the right elements
@@ -106,21 +109,79 @@ document.addEventListener('DOMContentLoaded', function(){
 
     function goToAmazon(){
 
-        switch(true){
-            case style.size == "m330" :
+        switch(style.size){
+            case "m330" :
+                switch(style.name){
+                    case "aethera" :
+                        window.open('https://www.amazon.com/Monolith-M-330-Anti-Fray-13x13x0-2-330x330x5mm/dp/B07S46PDSJ/ref=sr_1_25?keywords=monolith+mouse+pads&qid=1569941854&sr=8-25', '_blank');
+                        break;
+                    case "classic" :
+                        window.open('https://www.amazon.com/Monolith-M-330-Anti-Fray-13x13x0-2-330x330x5mm/dp/B07S5PVKT7/ref=sr_1_7?keywords=monolith+mouse+pads&qid=1569941854&sr=8-7', '_blank');
+                        break;
+                    case "clova" :
+                        window.open('https://www.amazon.com/Monolith-M-330-Anti-Fray-13x13x0-2-330x330x5mm/dp/B07RZW94GS/ref=sr_1_14?keywords=monolith+mouse+pads&qid=1569944963&sr=8-14', '_blank');
+                        break;
+                    case "nightSchool" :
+                        window.open('https://www.amazon.com/Monolith-M-330-NIGHTSCHOOL-Anti-Fray-330x330x5mm/dp/B07S33RG38/ref=sr_1_15?keywords=monolith+mouse+pads&qid=1569945033&sr=8-15', '_blank');
+                        break;
+                    case "redShift" :
+                        window.open('https://www.amazon.com/Monolith-M-330-Anti-Fray-13x13x0-2-330x330x5mm/dp/B07S472PFW/ref=sr_1_10?keywords=monolith+mouse+pads&qid=1569941854&sr=8-10', '_blank');
+                        break;
+                    case "slime" :
+                        window.open('https://www.amazon.com/Monolith-M-330-Anti-Fray-13x13x0-2-330x330x5mm/dp/B07S467G8D/ref=sr_1_18?keywords=monolith+mouse+pads&qid=1569944886&sr=8-18', '_blank');
+                        break;
+                    case "smokeScreen" :
+                        window.open('https://www.amazon.com/Monolith-M-330-Smokescreen-Anti-Fray-330x330x5mm/dp/B07S6SQ2BB/ref=sr_1_13?keywords=monolith+mouse+pads&qid=1569941854&sr=8-13', '_blank');
+                        break;
+                    case "summerSchool" :
+                        window.open('https://www.amazon.com/Monolith-M-330-SUMMERSCHOOL-Anti-Fray-330x330x5mm/dp/B07S58M1NP/ref=sr_1_17?keywords=monolith+mouse+pads&qid=1569941854&sr=8-17', '_blank');
+                        break;
+                    case "wakeUpCall" :
+                        window.open('https://www.amazon.com/Monolith-M-330-Anti-Fray-13x13x0-2-330x330x5mm/dp/B07S1YS733/ref=sr_1_11?keywords=monolith+mouse+pads&qid=1569941854&sr=8-11', '_blank');
+                        break;
+                }
                 break;
-            case style.size == "m508" :
+            case "m508" :
+                switch(style.name){
+                    case "aethera" :
+                        window.open('https://www.amazon.com/Monolith-M-508-Anti-Fray-20x16x0-2-508x406x5mm/dp/B07S3F5M9R/ref=sr_1_16?keywords=monolith+mouse+pads&qid=1569941854&sr=8-16', '_blank');
+                        break;
+                    case "classic" :
+                        window.open('https://www.amazon.com/Monolith-M-508-Anti-Fray-20x16x0-2-508x406x5mm/dp/B07RRRWMBR/ref=sr_1_1?keywords=monolith+mouse+pads&qid=1569941836&sr=8-1', '_blank');
+                        break;
+                    case "clova" :
+                        window.open('https://www.amazon.com/Monolith-M-508-Anti-Fray-20x16x0-2-508x406x5mm/dp/B07S4HLNZF/ref=sr_1_8?keywords=monolith+mouse+pads&qid=1569941854&sr=8-8', '_blank');
+                        break;
+                    case "nightSchool" :
+                        window.open('https://www.amazon.com/Monolith-M-508-NIGHTSCHOOL-Anti-Fray-508x406x5mm/dp/B07S16KHWW/ref=sr_1_9?keywords=monolith+mouse+pads&qid=1569941854&sr=8-9', '_blank');
+                        break;
+                    case "redShift" :
+                        window.open('https://www.amazon.com/Monolith-M-508-Anti-Fray-20x16x0-2-508x406x5mm/dp/B07S3DWGT5/ref=sr_1_24?keywords=monolith+mouse+pads&qid=1569941854&sr=8-24', '_blank');
+                        break;
+                    case "slime" :
+                        window.open('https://www.amazon.com/Monolith-M-508-Anti-Fray-20x16x0-2-508x406x5mm/dp/B07S16XVKG/ref=sr_1_5?keywords=monolith+mouse+pads&qid=1569941854&sr=8-5', '_blank');
+                        break;
+                    case "smokeScreen" :
+                        window.open('https://www.amazon.com/Monolith-M-508-Anti-Fray-20x16x0-2-508x406x5mm/dp/B07S16XVKG/ref=sr_1_5?keywords=monolith+mouse+pads&qid=1569941854&sr=8-5', '_blank');
+                        break;
+                    case "summerSchool" :
+                        window.open('https://www.amazon.com/Monolith-M-508-SUMMERSCHOOL-Anti-Fray-508x406x5mm/dp/B07RZ44S9W/ref=sr_1_19?keywords=monolith+mouse+pads&qid=1569941854&sr=8-19', '_blank');
+                        break;
+                    case "wakeUpCall" :
+                        window.open('https://www.amazon.com/Monolith-M-508-Anti-Fray-20x16x0-2-508x406x5mm/dp/B07S16L4L8/ref=sr_1_12?keywords=monolith+mouse+pads&qid=1569941854&sr=8-12', '_blank');
+                        break;
+                }
                 break;
         };
-        
+
      // classic m-508      : https://www.amazon.com/Monolith-M-508-Anti-Fray-20x16x0-2-508x406x5mm/dp/B07RRRWMBR/ref=sr_1_1?keywords=monolith+mouse+pads&qid=1569941836&sr=8-1
      // classic m-330      : https://www.amazon.com/Monolith-M-330-Anti-Fray-13x13x0-2-330x330x5mm/dp/B07S5PVKT7/ref=sr_1_7?keywords=monolith+mouse+pads&qid=1569941854&sr=8-7  
      // slime m-508        : https://www.amazon.com/Monolith-M-508-Anti-Fray-20x16x0-2-508x406x5mm/dp/B07S16XVKG/ref=sr_1_5?keywords=monolith+mouse+pads&qid=1569941854&sr=8-5
-     // slime m-330        : https://www.amazon.com/Monolith-M-508-Anti-Fray-20x16x0-2-508x406x5mm/dp/B07S16XVKG/ref=sr_1_5?keywords=monolith+mouse+pads&qid=1569941854&sr=8-5
+     // slime m-330        : https://www.amazon.com/Monolith-M-330-Anti-Fray-13x13x0-2-330x330x5mm/dp/B07S467G8D/ref=sr_1_18?keywords=monolith+mouse+pads&qid=1569944886&sr=8-18
      // smokeScreen m-508  : https://www.amazon.com/Monolith-M-508-Anti-Fray-20x16x0-2-508x406x5mm/dp/B07S16XVKG/ref=sr_1_5?keywords=monolith+mouse+pads&qid=1569941854&sr=8-5
      // smokeScreen m-330  : https://www.amazon.com/Monolith-M-330-Smokescreen-Anti-Fray-330x330x5mm/dp/B07S6SQ2BB/ref=sr_1_13?keywords=monolith+mouse+pads&qid=1569941854&sr=8-13
      // clova m-508        : https://www.amazon.com/Monolith-M-508-Anti-Fray-20x16x0-2-508x406x5mm/dp/B07S4HLNZF/ref=sr_1_8?keywords=monolith+mouse+pads&qid=1569941854&sr=8-8
-     // clova m-330        : https://www.amazon.com/Monolith-M-330-Anti-Fray-13x13x0-2-330x330x5mm/dp/B07RZW94GS/ref=sr_1_14?keywords=monolith+mouse+pads&qid=1569941854&sr=8-14
+     // clova m-330        : https://www.amazon.com/Monolith-M-330-Anti-Fray-13x13x0-2-330x330x5mm/dp/B07RZW94GS/ref=sr_1_14?keywords=monolith+mouse+pads&qid=1569944963&sr=8-14
      // nightSchool m-508  : https://www.amazon.com/Monolith-M-508-NIGHTSCHOOL-Anti-Fray-508x406x5mm/dp/B07S16KHWW/ref=sr_1_9?keywords=monolith+mouse+pads&qid=1569941854&sr=8-9
      // nightSchool m-330  : https://www.amazon.com/Monolith-M-330-NIGHTSCHOOL-Anti-Fray-330x330x5mm/dp/B07S33RG38/ref=sr_1_15?keywords=monolith+mouse+pads&qid=1569941854&sr=8-15
      // redShift m-330     : https://www.amazon.com/Monolith-M-330-Anti-Fray-13x13x0-2-330x330x5mm/dp/B07S472PFW/ref=sr_1_10?keywords=monolith+mouse+pads&qid=1569941854&sr=8-10
